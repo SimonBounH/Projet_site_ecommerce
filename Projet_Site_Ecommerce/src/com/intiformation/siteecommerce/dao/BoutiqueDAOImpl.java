e835b8043c94fa2da4497953382980925c9ca6d1
Anthony Sauvage <antho.sauvage@gmail.com>
2019-05-02 19:39:36 +0200

    couche DAO avec instanciation sessionFactory
    couche service avec applicationContext-dao

--- Projet_Site_Ecommerce/src/com/intiformation/siteecommerce/dao/BoutiqueDAOImpl.java
+++ Projet_Site_Ecommerce/src/com/intiformation/siteecommerce/dao/BoutiqueDAOImpl.java
@@ -1,5 +1,135 @@
 package com.intiformation.siteecommerce.dao;
 
-public interface BoutiqueDAOImpl {
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.intiformation.siteecommerce.modele.Categorie;
+import com.intiformation.siteecommerce.modele.Client;
+import com.intiformation.siteecommerce.modele.Commande;
+import com.intiformation.siteecommerce.modele.Panier;
+import com.intiformation.siteecommerce.modele.Produit;
+import com.intiformation.siteecommerce.modele.Role;
+import com.intiformation.siteecommerce.modele.User;
+
+/**
+ * 
+ * @author IN-MP-003
+ *
+ */
+@Repository
+public class BoutiqueDAOImpl implements IBoutiqueDAO {
+
+	// déclaration de la session factory d'hibernate
+	@Autowired // injection automaatique de la session factory
+	private SessionFactory sessionFactory;
+	
+	/**
+	 * setter de la sessionFactory pour permettre au conteneur 
+	 * spring l'injection <br/>
+	 * @param sessionFactory
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	
+	@Override
+	public Long ajouterCategorie(Categorie c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Categorie> listCategorie() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categorie getCategorie(Long idCat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimerCategrorie(Long idcat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifierCategorie(Categorie c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long ajouterProduit(Produit p, Long idCat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produit> listproduits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produit> produitsParMotCle(String mc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produit> produitsParCategorie(Long idCat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Produit> produitsSelectionnes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Produit getProduit(Long idP) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimerProduit(Long idP) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifierProduit(Produit p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ajouterUser(User u) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attribuerRole(Role r, Long userID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Commande enregistrerCommande(Panier p, Client c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
