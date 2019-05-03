dbef6731e185482a65bb47c50f9af6f1cd293a0d
Anthony Sauvage <antho.sauvage@gmail.com>
2019-05-03 17:21:35 +0200

    Aucune idée

---
+++ Projet_Site_Ecommerce/src/com/intiformation/siteecommerce/service/IAdminCategoriesMetier.java
@@ -0,0 +1,9 @@
+package com.intiformation.siteecommerce.service;
+
+import com.intiformation.siteecommerce.modele.User;
+
+public interface IAdminCategoriesMetier {
+	
+	public void ajouterUser(User u);
+
+}
