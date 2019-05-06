f45fe9082e874cae9649585624b4297adb1f3bb8
Simon BounHouang <bounhouang.simon@gmail.com>
2019-05-06 15:09:11 +0200

    Modif applicationContext + web.xml Suppression modèles

--- Projet_Site_Ecommerce/src/com/intiformation/siteecommerce/service/IAdminCategoriesMetier.java
+++
@@ -1,9 +0,0 @@
-package com.intiformation.siteecommerce.service;
-
-import com.intiformation.siteecommerce.modele.User;
-
-public interface IAdminCategoriesMetier {
-	
-	public void ajouterUser(User u);
-
-}
